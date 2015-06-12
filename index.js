9255d810ab595231da2fab5e6ed12033f580943d
Brian Woodward <brian.woodward@gmail.com>
2015-06-12 17:51:37 -0400

    init

---
+++ index.js
@@ -0,0 +1,12 @@
+/*!
+ * iterator-sync <https://github.com/doowb/iterator-sync>
+ *
+ * Copyright (c) 2015, Brian Woodward.
+ * Licensed under the MIT License.
+ */
+
+'use strict';
+
+module.exports = function () {
+  // do stuff
+};
