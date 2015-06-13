97bb1fefe347c7bd35d872c5eaa9f4b2f9e6743e
Brian Woodward <brian.woodward@gmail.com>
2015-06-12 20:54:59 -0400

    adding another test

--- test.js
+++ test.js
@@ -4,22 +4,33 @@ var assert = require('assert');
 var iterator = require('./');
 
 describe('iterator-sync', function () {
+  it('should create an iterator function when given a stack', function () {
+    var called = [];
+    var stack = getStack(called);
+    var fn = iterator(stack);
+    assert.equal(typeof fn, 'function');
+  });
+
   it('should iterate over a stack of functions', function () {
     var called = [];
-    var stack = [
-      function a (key, value) {
-        called.push('a');
-        var obj = {};
-        obj[key] = value;
-        return obj;
-      },
-      function b (obj) { called.push('b'); return obj; },
-      function c (obj) { called.push('c'); return obj; },
-      function d (obj) { called.push('d'); return obj; },
-      function e (obj) { called.push('e'); return obj; }
-    ];
-
+    var stack = getStack(called);
     assert.deepEqual(iterator(stack)('foo', 'bar'), { foo: 'bar' });
     assert.deepEqual(called, ['a', 'b', 'c', 'd', 'e']);
   });
 });
+
+function getStack (called) {
+  var stack = [
+    function a (key, value) {
+      called.push('a');
+      var obj = {};
+      obj[key] = value;
+      return obj;
+    },
+    function b (obj) { called.push('b'); return obj; },
+    function c (obj) { called.push('c'); return obj; },
+    function d (obj) { called.push('d'); return obj; },
+    function e (obj) { called.push('e'); return obj; }
+  ];
+  return stack;
+}
