5d431baf50e813baeeaa16713b7406baef4844ba
Brian Woodward <brian.woodward@gmail.com>
2015-06-16 00:05:49 -0400

    Rebuilding site 2015-06-16T04:05:48.543Z

--- example.js
+++
@@ -1,9 +0,0 @@
-var fs = require('fs');
-var iterator = require('./');
-var stack = [
-  function (fp) { return fs.readFileSync(fp, 'utf8'); },
-  function (contents) { return JSON.parse(contents); }
-];
-var readJSON = iterator(stack);
-var pkg = readJSON('./package.json');
-console.log(pkg);
