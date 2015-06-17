4b748cf2e09089bb4b38eb9bc16ed5780ab725cc
jonschlinkert <jon.schlinkert@sellside.com>
2015-06-17 16:50:42 -0400

    minor edits

--- example.js
+++ example.js
@@ -1,9 +1,15 @@
 var fs = require('fs');
 var iterator = require('./');
+
 var stack = [
-  function (fp) { return fs.readFileSync(fp, 'utf8'); },
-  function (contents) { return JSON.parse(contents); }
+  function (fp) {
+    return fs.readFileSync(fp, 'utf8');
+  },
+  function (contents) {
+    return JSON.parse(contents);
+  }
 ];
+
 var readJSON = iterator(stack);
 var pkg = readJSON('./package.json');
 console.log(pkg);
